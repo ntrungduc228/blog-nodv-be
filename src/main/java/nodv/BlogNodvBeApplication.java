f71f0acc0e469eb57a95a36c3177c7ad29bc3c42
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2022-11-26 11:25:47 +0700

    feat: Enable MongoAuditing for using class AuditMetadata

--- src/main/java/nodv/BlogNodvBeApplication.java
+++ src/main/java/nodv/BlogNodvBeApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication()
 @EnableConfigurationProperties(AppProperties.class)
+@EnableMongoAuditing
 public class BlogNodvBeApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogNodvBeApplication.class, args);
