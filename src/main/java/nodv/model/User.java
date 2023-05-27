2af56310c7def947d18ee393d3134983d9fa648b
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2023-05-27 14:03:55 +0700

    Fix switch statement indentation and add missing space in AdminController, and import ArrayList in User model

--- src/main/java/nodv/model/User.java
+++ src/main/java/nodv/model/User.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
